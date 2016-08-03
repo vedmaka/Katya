05be7955b41881a410055954700c0290c4b40234
Vedmaka <god.vedmaka@gmail.com>
2016-08-03 13:58:07 +0300

    gitignore updated

---
+++ app/Http/Controllers/VkCallbackController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class VkCallbackController extends Controller
+{
+
+	public function index()
+	{
+		return response('ff0fe0bc');
+	}
+
+}
